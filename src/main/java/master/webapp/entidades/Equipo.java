335a671b722594c95b4ef10d7206751ed82f8d21
rajila <rdajila@gmail.com>
2024-11-27 20:22:49 +0100

    changes: mapeo equipo y piloto jpa

--- src/main/java/master/webapp/entidades/Equipo.java
+++ src/main/java/master/webapp/entidades/Equipo.java
@@ -1,5 +1,26 @@
 package master.webapp.entidades;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "equipo")
 public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "logo")
+    private byte[] logo;
+
+    @Column(name = "twitter")
+    private String twitter;
 
 }
