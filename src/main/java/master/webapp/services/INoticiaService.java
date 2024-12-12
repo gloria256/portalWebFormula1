e3d8139ae0b7f683e49d6753e66f49b1d5e601b2
Enzo Viacava <viacava.enzo@gmail.com>
2024-12-12 12:59:43 +0000

    Se crea interfaz e implementacion para func. Noticia, se añade delete

---
+++ src/main/java/master/webapp/services/INoticiaService.java
@@ -0,0 +1,13 @@
+package master.webapp.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import master.webapp.entidades.Noticia;
+
+public interface INoticiaService {
+    List<Noticia> getAllNoticias();
+    Optional<Noticia> findNoticiaById(Integer id);
+    Optional<Noticia> deleteNoticiaById(Integer id);
+}
